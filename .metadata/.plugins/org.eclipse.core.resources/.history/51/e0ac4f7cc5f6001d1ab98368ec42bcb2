f75c67f2094ba0378b016963d563e8a4502123db
Karin0616 <xhdsxhd@naver.com>
2023-05-20 14:22:13 +0900

    skill 계산
    스킬계산이랑 레벨계산 만들어놨음

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/51/e0ac4f7cc5f6001d1ab98368ec42bcb2
@@ -0,0 +1,14 @@
+
+public class Skill extends Pokemon{
+	protected float skill_Damage;
+	protected float skill_debuff;
+	public void useskill(Skill_Name skill) {
+		
+		switch (skill) {
+		case 몸통박치기:
+			skill_Damage = 10;
+			break;
+		
+		}
+	}
+}
--- PokemonProject/src/Pokemon.java
+++ PokemonProject/src/Pokemon.java
@@ -11,6 +11,7 @@ public class Pokemon extends UserData {
 	protected int eveLv;
 	protected int[] skillIndexNum = new int[4];
 	public int add = 0;
+	public int exp =0;
 	PokemonDex pdex[] = PokemonDex.getInstance();
 	public Pokemon() {
 		
@@ -35,5 +36,49 @@ public class Pokemon extends UserData {
 		this.DEF=((2*def)+add)*this.Lv/100;
 		this.SPD=((2*spd)+add)*this.Lv/100;
 		
+	}
+	public void lvCalculrator(int[] Candy) {
+		for(int i=0;i<5;i++) {
+			if(Candy[i]==0) {
+				continue;
+			}else {
+				switch(i) {
+				case 0:
+					this.exp+=100;
+				case 1:
+					this.exp+=200;
+				case 2:
+					this.exp+=300;
+				case 3:
+					this.exp+=400;
+				case 4:
+					this.exp+=500;
+				}
+				
+			}
+		}
+		if(exp>=100&&this.Lv<10) {
+			this.Lv+=exp/100;
+		}else if (exp>=250&&this.Lv<20) {
+			this.Lv+=exp/250;
+		}else if (exp>=350&&this.Lv<30) {
+			this.Lv+=exp/350;
+		}else if (exp>=450&&this.Lv<40) {
+			this.Lv+=exp/450;
+		}else if (exp>=600&&this.Lv<50) {
+			this.Lv+=exp/600;
+		}else if (exp>=800&&this.Lv<60) {
+			this.Lv+=exp/800;
+		}else if (exp>=1000&&this.Lv<70) {
+			this.Lv+=exp/1000;
+		}else if (exp>=1200&&this.Lv<80) {
+			this.Lv+=exp/1200;
+		}else if (exp>=1500&&this.Lv<100) {
+			if(this.Lv+exp/1500>100) {
+				
+			}else {
+				this.Lv+=exp/1500;
+			}
+		}
 	}
 }
